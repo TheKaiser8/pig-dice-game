1c46a645482207cd695a31eafbb448d4c7b2c755
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-04-07 15:24:38 +0200

    Add rolling dice functionality

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 'use strict';
 
-// Selecting elements
+// Seleziono elementi
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-// Setting elements score = 0
-score0El.textContent = 0; // per visualizzarli in pagina JS li convertirà in stringhe
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Imposto condizioni iniziali
+score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden'); // nascondo img dado
 
-diceEl.classList.add('hidden');
+let currentScore = 0;
+
+// Implemento funzionalità lancio dadi
+btnRoll.addEventListener('click', function () {
+  // 1. Genero numero di dadi casuale da 1 a 6
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+
+  // 2. Visualizzo dado con l'immagine corrispondente al numero uscito
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceNumber}.png`; // imposto l'immagine del dado da visualizzare (caricamento dinamico img)
+
+  // 3. Controllo se il numero uscito è = 1: se true passo al giocatore successivo
+  if (diceNumber !== 1) {
+    // Aggiungo il numero uscito al punteggio corrente e lo visualizzo in pagina
+    currentScore += diceNumber;
+    current0El.textContent = currentScore;
+  } else {
+    // Riporto a 0 il punteggio corrente
+    currentScore = 0;
+    current0El.textContent = currentScore;
+  }
+});
